1d324a876f67cdb67b9085a81f4bb8e1e479323a
CerberusOne <aingaran2003@gmail.com>
2017-02-12 23:49:23 -0800

    input complete

--- a2.c
+++ a2.c
@@ -54,7 +54,6 @@ typedef struct
 	char mesg_data[MAXMESSAGEDATA];	//message content
 }Mesg;
 
-
 #define MESGHDRSIZE (sizeof(Mesg) - MAXMESSAGEDATA )
 
 //sets up the server process, client/server processes and waits for signals from the IPC Channel
@@ -62,7 +61,12 @@ int serverSetup(key_t mkey, int msg_id);
 
 //sets up the client process
 int clientSetup(int msg_id);
-
+void promptUser(char *prompt, char *filename);
+Mesg createMesg(long type, char mesg_data[MAXMESSAGEDATA]);
+char* createMesgData(char* pidChar, char* filename);
+void processRequest(Mesg message, long* pid, char* filename, long* priority);
+void getMesgPid(Mesg message, long* pid);
+void copyStr(char str1[], char str2[]);
 
 
 int main(int argc, char *argv[]){
@@ -101,9 +105,12 @@ int main(int argc, char *argv[]){
 
 	//setup server
 int serverSetup(key_t mkey, int msg_id){
-	int permflags;	//holds the result of msgget
+	//int permflags;	//holds the result of msgget
 	int result, length;
-	Mesg mesgbuf;
+	Mesg message;
+	char filename[1024];
+	long pid = 0;
+	long priority = 0;
 
 	//the length is essetnailly the size of the structure minus sizeof(mtype)
 	length = sizeof(Mesg) - sizeof(long);
@@ -115,15 +122,25 @@ int serverSetup(key_t mkey, int msg_id){
 	//While not EOF	
 	while(1){
 		//While there's input in the Message queue
-		//
 		printf("checking msgrcv\n");
-		if((result = msgrcv(msg_id, &mesgbuf, length, 0, 0)) == -1){
+
+		//change to signal later
+		if((result = msgrcv(msg_id, &message, length, 0, 0)) == -1){
 			printf("Failed: msgrcv %d\n", result);
 			return -1;
 		}
-		printf("Successful: msgrcv %d\n", result);
 
-			//Loop through every object
+		printf("Successful: msgrcv %d\n", result);
+		printf("Message(%ld): %s\n", strlen(message.mesg_data), message.mesg_data);
+
+		//get the pid, filename(data) and priority from the message
+		processRequest(message, &pid, filename, &priority);
+
+		printf("pid: %ld\nfilename: %s\npriority: %ld\n", pid, filename, priority);
+
+
+
+		//Loop through every object
 				//if the type_priority is not null - ASK
 					//if the priority is higher than the highest found
 						//save the object's index in the list
@@ -135,6 +152,8 @@ int serverSetup(key_t mkey, int msg_id){
 				//print error: found message with no PID
 			//otherwise, Save the message data
 				//Open a file with the name of the message data
+
+
 				//if the fails to open
 					//if it can't be found
 						//Save the message as "File can't be found"
@@ -154,70 +173,134 @@ int serverSetup(key_t mkey, int msg_id){
 	return 0;
 }
 
+
+void processRequest(Mesg message, long* pid, char* filename, long* priority){
+	char* temp = message.mesg_data;
+	char* pidChar;
+	int i = 0;
+	char* ptr;
+
+	*priority = message.mesg_type;	
+
+	while(temp){
+		pidChar = strchr(temp, '\n');
+
+		//change the new line character to null
+		if(pidChar != '\0'){
+			*pidChar = '\0';
+		}
+
+		if(i == 0)
+			*pid = strtol(temp, &ptr, 10);
+		if(i == 1)
+			strcpy(filename, temp);
+
+		
+		//fix the new line character back to normal
+		if(pidChar != '\0'){
+			*pidChar = '\n';
+		}
+	
+		temp = pidChar ? (pidChar + 1) : NULL;
+		i++;
+	}	
+}
+
 //setup client
 int clientSetup(int msg_id){
-	int size, flags, retval;
-	char* quit = "q";
-	char filename[1024];
-	char* priorityChar;
-	long priority;
+	//int size, flags, retval;
+	char quit[] = "q";			//command used to exit program
+	char filename[1024];			//filename of requested file to get
+	char priorityChar[1024];	//priority of current message 
+	long priority;				//priority of current message after being converted to long
+	long pid;					//pid of current thread
+	char pidChar[1024];			//pid of current thread in char* form
+	int result;					//send the message to the IPC Message queue
+	char mesg_data[MAXMESSAGEDATA];
+
 
 	//Create a thread
 		//if an event occurs in the IPC Message queue 
 			//if the new message's type_PID is the same as this process's PID
 				//print the message data
-	//while not EOF
-	while(1){
-		//Prompt the user for a filename
-		printf("Enter input (q quit):\n");
 
-		//save the filename
-		gets(filename);
+	//user input loop
+	while(1){
+		//prompt for filename from user
+		promptUser("Enter input (q quit): ", filename);
 		
 		//kill threads and exit
 		if(strcmp(filename, quit) == 0){
-			printf("Exiting...");
+			printf("Exiting...\n");
 			return 0;
 		}
+	
 		//Prompt for the message's priority
-		printf("Enter priority (1max - 3min): \n");
+		promptUser("Enter priority (1max - 3min): ", priorityChar);
+		//conver input into a long
+		priority = atol(priorityChar);
 
-		//save the priority
-		gets(priorityChar);
-		atoi(priorityChar);
-
-		//if the priority is between 1 and 10
 		if((priority >= 1) && (priority <= 3)){
-			//Save this process's PID
+			//get pid and save as long
+			pid = getpid();
+			sprintf(pidChar, "%ld", pid);
+	
+			//create message data
+			strcpy(mesg_data, createMesgData(pidChar, filename));			
+
 			//Create a message object
-			Mesg message;
-			char hello[MAXMESSAGEDATA] = "hello";
-			strcpy(message.mesg_data, hello);
-			message.mesg_type = 1;
-			message.mesg_len = strlen(message.mesg_data);
+			Mesg message = createMesg(1, mesg_data);		
 
-				
-				//length is the length of the file's conent
-				//type_PID is this process's PID
-				//type_priority is the priority set by the user
-				//message data is filename set by the user
-			//send the message to the IPC Message queue
-			int result, length;
-
-			//size of the message structure minus the type 
-			length = message.mesg_len;
-			//check if the third parameter (length) is correct. Might be sizeof(message)-size(long)
-			if((result = msgsnd(msg_id, &message, length, 0) == -1))
+			//check if message gets sent properly
+			if((result = msgsnd(msg_id, &message, message.mesg_len, 0) == -1))
 			{
 				printf("Failed: msgsnd %d\n", result);
 				return (-1);
 			}
-			printf("Successful: msgsnd %d\n", result);
-		//otherwise, 
-			//print "priority must be between 1 and 10"
+			printf("Successful: data: %s\n", message.mesg_data);
 		}
-		
+		else{
+			//alert user that priority is not properly defined
+			printf("ERROR: Priority must be between 1 (max) and 3 (min)\n");
+		}
+	
 	}
 
 	return 0;
+}
+
+void promptUser(char *prompt, char *filename){
+	fflush(stdin);
+
+	//Prompt the user for a filename
+	printf("%s", prompt);
+
+	//save the filename
+	fgets(filename, 32, stdin);
+	//get rid of the new line character generated by fgets
+	filename[strcspn(filename, "\n")] = '\0';
+}
+
+//length is the length of the file's conent
+//message data is filename set by the user
+Mesg createMesg(long type, char mesg_data[1024]){
+	fflush(stdin);
+	Mesg message;
+	message.mesg_type = type;
+	strcpy(message.mesg_data, mesg_data);
+	//message.mesg_len = strlen(message.mesg_data);
+	message.mesg_len = sizeof(Mesg) - sizeof(long);
+
+	return message;
+}
+
+char* createMesgData(char* pidChar, char* filename){
+	//string to be properly formatted for a Mesg's mesg_data attribute
+	char *mesg_data;
+
+	//format with pid on first line and rest of message on another
+	mesg_data = strcat(pidChar, "\n");
+	strcat(mesg_data, filename);
+
+	return mesg_data;
 }
